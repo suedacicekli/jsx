90e46f29f5c5323b5adff7e29e076e517d51d2c9
suedacicekli <suedacicekli@gmail.com>
2023-02-16 14:50:47 +0300

    udemyReactEducation | Start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+// 1.Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2.Get a reference to the div with ID root
+const re = document.getElementById('root');
+// 3. Tell React to take control of that element
+const root = ReactDOM.createRoot(re);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 4.Create a component
+function App() {
+    const name = 'Sueda';
+    const age = 23;
+
+    return (<p>Hello my name is {name} , I'm {age} years old</p>);
+}
+// 5. Show the component on the screen
+root.render(<App />);
